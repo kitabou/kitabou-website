1f67708a5a9eb78b261066217d3375791e24c64c
Masaki Yoshida <colorqualia@gmail.com>
2015-02-27 17:35:18 +0900

    プライバシーポリシーにモーダルを実装

--- app/js/main.js
+++ app/js/main.js
@@ -61,3 +61,22 @@ $(".page-menu__sp-menu").click(function (e) {
     console.log(e.type);
     $(this).next().slideToggle();
 }).next().hide();
+
+// modal
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-window").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
