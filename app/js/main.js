d0d6aa461d247607c6fb557342c983a664210c06
Masaki Yoshida <colorqualia@gmail.com>
2015-05-18 18:57:31 +0900

    Release
     - devブランチのコミットを一つにまとめたコミット

---
+++ app/js/main.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+
+    // jQuery.BgSwitcher
+    $(".mainVisual").bgswitcher({
+      images: ["../images/pic1.jpg", "../images/pic2.jpg", "../images/pic3.jpg"]
+    });
+
+    // navigation-menu
+    $('#js-centered-navigation-menu').removeClass("show");
+
+    $('#js-centered-navigation-mobile-menu').on('click', function(e) {
+        e.preventDefault();
+        $('#js-centered-navigation-menu').slideToggle(function() {
+            if ($('#js-centered-navigation-menu').is(':hidden')) {
+                $('#js-centered-navigation-menu').removeAttr('style');
+            }
+        });
+    });
+});
+
+// gmaps
+var map = new GMaps({
+    el: '#map',
+    lat: 35.581196,
+    lng: 140.163724,
+    zoom: 16,
+    scrollwheel: false,
+    draggable: false
+});
+map.addMarker({
+    lat: 35.581196,
+    lng: 140.163724,
+    title: "株式会社北原防災"
+});
+
+// scrollUp
+$(function() {
+    $.scrollUp({
+        scrollText: '<i class="fa fa-chevron-up"></i>'
+    });
+});
+
+// items bannner
+var loop = setInterval(function() {
+    //li先頭要素のクローンを作成
+    var clone = $(".items-banner li:first").clone(true);
+    //li先頭要素のマージントップにマイナスを指定しアニメーションさせる
+    $(".items-banner li:first").animate({
+        marginTop: "-95px"
+    }, {
+        duration: 500,
+        complete: function() {
+            //処理完了時に先頭要素を削除
+            $(".items-banner li:first").remove();
+            //クローンをliの最後に追加
+            clone.clone(true).insertAfter($(".items-banner li:last"));
+        }
+    });
+}, 3000);
+
+// page-menu__sp-menu
+$(".page-menu__sp-menu").click(function (e) {
+    console.log(e.type);
+    $(this).next().slideToggle();
+}).next().hide();
+
+// modal
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-window").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
---
+++ gulpfile.js
@@ -0,0 +1,130 @@
+var gulp = require('gulp');
+var pleeease = require('gulp-pleeease');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-ruby-sass');
+var csscomb = require('gulp-csscomb');
+var csso = require('gulp-csso');
+var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var size = require('gulp-size');
+var hb = require('gulp-hb');
+var frontMatter = require('gulp-front-matter');
+var uncss = require('gulp-uncss');
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: "app"
+        },
+        tunnel: true,
+    });
+});
+
+// style task
+gulp.task('style', function() {
+    return gulp.src('app/sass/{,*/}*.scss')
+        .pipe(plumber())
+        .pipe(sass({
+            "sourcemap=none": true
+        }))
+        .pipe(pleeease({
+            autoprefixer: {
+                browsers: ['last 2 versions'],
+                cascade: false
+            },
+            minifier: false,
+        }))
+        .pipe(csscomb())
+        .pipe(size())
+        .pipe(gulp.dest('app/css'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+// CSS Optimize task
+gulp.task('csso', function() {
+    console.log("CSS Optimized!");
+    return gulp.src('app/css/style.css')
+        .pipe(uncss({
+          html:['app/**/*.html', '!app/templates/**/*.html'],
+          ignore: ['#scrollUp']
+          }))
+        .pipe(csso())
+        .pipe(gulp.dest('dist/css'));
+});
+
+// imagemin task
+gulp.task('imagemin', function() {
+    return gulp.src('app/images/**/*.+(jpg|png|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+});
+
+// minify JS
+gulp.task('uglify', function() {
+    return gulp.src('app/js/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'));
+});
+
+// minify HTML
+gulp.task('minify-html', function() {
+    var opts = {
+        comments: true,
+        spare: true
+    };
+
+    return gulp.src(['app/**/*.html', '!app/templates/**/*.html'])
+        .pipe(minifyHTML(opts))
+        .pipe(gulp.dest('dist'));
+});
+
+// handlebars task
+gulp.task('hb', function() {
+    return gulp.src('app/templates/pages/**/*.html')
+        .pipe(frontMatter())
+        .pipe(hb({
+            // data: '',
+            // helpers: [],
+            partials: [
+                'app/templates/partials/*.hbs'
+            ]
+        }))
+        .pipe(gulp.dest('app'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+// watch task
+gulp.task('watch', ['browserSync'], function() {
+    gulp.watch(['app/sass/{,*/}*.scss'], ['style']);
+    gulp.watch(['app/images/**/*.+(jpg|png|gif|svg)'], reload);
+    gulp.watch(['app/templates/pages/**/*.html', 'app/templates/partials/*.hbs'], ['hb']);
+});
+
+gulp.task('optimize', function() {
+    runSequence(
+        'csso', // 1
+        'minify-html' // 2
+    );
+});
+
+// build task
+gulp.task('build', ['optimize', 'imagemin', 'uglify']);
+
+// clean task
+gulp.task('clean', function(cb) {
+    del(['dist'], cb);
+});
+
+// default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('build');
+});
